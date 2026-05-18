9ed2de0127bf9a8052332821c640b3b2ca09a51a
kauas3228 <kauas3228@gmail.com>
2026-05-17 22:40:28 -0300

    fix: disable scroll in active mobile nav-bar

--- frontend/dist/scripts/homeScript.js
+++ frontend/dist/scripts/homeScript.js
@@ -4,12 +4,14 @@ const navMenu = document.getElementById('nav-mobile-menu');
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle('active');
     navMenu.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
 });
 const links = document.querySelectorAll('.nav-link-item a');
 links.forEach(link => {
     link.addEventListener('click', () => {
         menuBtn.classList.remove('active');
         navMenu.classList.remove('active');
+        document.body.classList.remove('no-scroll');
     });
 });
 const buttons = document.querySelectorAll('.nav-button');
@@ -17,6 +19,7 @@ buttons.forEach(button => {
     button.addEventListener('click', () => {
         menuBtn.classList.remove('active');
         navMenu.classList.remove('active');
+        document.body.classList.remove('no-scroll');
     });
 });
 //# sourceMappingURL=homeScript.js.map
