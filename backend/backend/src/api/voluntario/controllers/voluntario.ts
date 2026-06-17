b7252a28438d9e8ae1f15de9a9acba1374af51ea
ikarofreitas <freitasikaro73@gmail.com>
2026-06-17 16:15:19 -0300

    feat: inicia strapi e cria um usuario

---
+++ backend/backend/src/api/voluntario/controllers/voluntario.ts
@@ -0,0 +1,7 @@
+/**
+ * voluntario controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::voluntario.voluntario');
---
+++ backend/backend/src/api/voluntario/routes/voluntario.ts
@@ -0,0 +1,7 @@
+/**
+ * voluntario router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::voluntario.voluntario');
